00a077a3475e7049eb2143b3ffeeaa30610ea18e
Rishitha <137534625+RishithaDuddolla@users.noreply.github.com>
2024-03-16 22:07:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ const Element = (
     <h1 class='heading'>Notifications</h1>
    <div>
     <Notification
-      className='Blue b'
+      className='Blue bgr'
       notify='Information Message'
       src='https://assets.ccbp.in/frontend/react-js/primary-icon-img.png'
     />
     <Notification
-      className='green'
+      className='green bgr'
       notify='Success Message'
       src='https://assets.ccbp.in/frontend/react-js/success-icon-img.png'
     />
     <Notification
-      className='yellow'
+      className='yellow bgr'
       notify='Warning Message'
       src='https://assets.ccbp.in/frontend/react-js/warning-icon-img.png'
     />
     <Notification
-      className='red'
+      className='red bgr'
       notify='Error Message'
       src='https://assets.ccbp.in/frontend/react-js/danger-icon-img.png'
     />
